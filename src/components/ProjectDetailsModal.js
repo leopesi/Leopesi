43b131bb86c587b4c42f87e5353c6b9d54d987c1
Leonardo Pereira <leopesi@yahoo.com.br>
2024-10-20 13:09:27 -0300

    created a modal to open imagem

--- src/components/ProjectDetailsModal.js
+++ src/components/ProjectDetailsModal.js
@@ -2,9 +2,33 @@ import React, { Component } from "react";
 import { Modal, Image } from 'react-bootstrap';
 
 class ProjectDetailsModal extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showImageModal: false,
+      selectedImage: null
+    };
+  }
+
+  // Função para abrir o modal com a imagem clicada
+  handleImageClick = (image) => {
+    this.setState({
+      showImageModal: true,
+      selectedImage: image
+    });
+  };
+
+  // Função para fechar o modal de imagem
+  handleCloseImageModal = () => {
+    this.setState({
+      showImageModal: false,
+      selectedImage: null
+    });
+  };
+
   render() {
- 
     const { title, url, text, videos, images, pdfs } = this.props.data;
+    const { showImageModal, selectedImage } = this.state;
 
     const renderVideos = () => {
       if (Array.isArray(videos)) {
@@ -57,7 +81,13 @@ class ProjectDetailsModal extends Component {
                 <h2 className="text-center" style={{ borderTop: "1px solid #ccc", margin: "2em " }}>
                   {item.titleImage}
                 </h2>
-                <Image src={item.image} fluid style={{ maxWidth: "100%" }} />
+                {/* Adicionando onClick para abrir o modal de imagem */}
+                <Image 
+                  src={item.image} 
+                  fluid 
+                  style={{ maxWidth: "100%", cursor: "pointer" }} 
+                  onClick={() => this.handleImageClick(item.image)}
+                />
               </div>
             );
           }
@@ -89,60 +119,100 @@ class ProjectDetailsModal extends Component {
     };
 
     return (
-      <Modal
-        {...this.props}
-        size="xl"
-        aria-labelledby="contained-modal-title-vcenter"
-        centered
-        fullscreen
-        dialogClassName="modal-fullscreen"
-      >
-        <span onClick={this.props.onHide} className="modal-close">
-          <i className="fas fa-times fa-3x close-icon"></i>
-        </span>
-       
-        <div className="col-md-12">
-
-          <div className="col-md-10 mx-auto">
-          <div className="modal-close">
- 
-              </div>
-            <Modal.Header>
-              <div>
-                <div className="git-icon">
-                        {url && (
-                          <a
-                            href={url}
-                            target="_blank"
-                            rel="noopener noreferrer"
-                            
-                            
-                          >
-                            <i class='fab fa-github fa-spin fa-3x'></i>
-                          </a>
-                        )}
-                </div>
-                <Modal.Title>
-                  <div className="large-font">
-                    {title}
+      <>
+        <Modal
+          {...this.props}
+          size="lg"
+          aria-labelledby="contained-modal-title-vcenter"
+          centered
+          fullscreen
+          dialogClassName="modal-fullscreen"
+        >
+          <span onClick={this.props.onHide} className="modal-close">
+            <i className="fas fa-times fa-3x close-icon"></i>
+          </span>
+         
+          <div className="col-md-12">
+            <div className="col-md-10 mx-auto">
+              <Modal.Header>
+                <div>
+                  <div className="git-icon">
+                    {url && (
+                      <a
+                        href={url}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        <i className='fab fa-github fa-spin fa-3x'></i>
+                      </a>
+                    )}
                   </div>
-                </Modal.Title>
-              </div>
-            </Modal.Header>
-            
-            <Modal.Body className="modal-body">
-              {renderText()}
-              {renderVideos()}
-              {renderImages()}
-              {renderPDFs()}
-            </Modal.Body>
-            <p className="modal-description"></p>
-            <div className="col-md-12 text-center">
-              <ul className="list-inline mx-auto"></ul>
+                  <Modal.Title>
+                    <div className="large-font">
+                      {title}
+                    </div>
+                  </Modal.Title>
+                </div>
+              </Modal.Header>
+              
+              <Modal.Body className="modal-body">
+                {renderText()}
+                {renderVideos()}
+                {renderImages()}
+                {renderPDFs()}
+              </Modal.Body>
             </div>
           </div>
-        </div>
-      </Modal>
+        </Modal>
+
+
+        <Modal 
+          show={showImageModal} 
+          onHide={this.handleCloseImageModal} 
+          centered 
+          dialogClassName="image-modal" // Personalização via classe CSS
+          size="lg" // Definir tamanho grande
+        >
+          {/* Ícone de fechar posicionado no canto superior direito */}
+          <span 
+            onClick={this.handleCloseImageModal} 
+            style={{
+
+              right: '10px',
+              cursor: 'pointer',
+
+            }}
+          >
+            <i className="fas fa-times fa-2x"></i>
+          </span>
+
+          {/* Ajuste no Modal.Body */}
+          <Modal.Body 
+            style={{
+              padding: 0, 
+              display: 'flex', 
+              justifyContent: 'center', 
+              alignItems: 'center',
+              overflow: 'hidden', // Remove rolagem extra no modal
+              maxHeight: '100vh' // Limita a altura máxima ao tamanho da viewport
+            }}
+          >
+            {/* Aumentar o tamanho da imagem, mas garantir que não ultrapasse a tela */}
+            <Image 
+              src={selectedImage} 
+              style={{ 
+                width: '100%', // A imagem ocupa toda a largura do modal
+                height: 'auto', // A altura é ajustada automaticamente para manter a proporção
+                maxHeight: '100vh' // A altura máxima é o tamanho da viewport
+              }} 
+            />
+          </Modal.Body>
+</Modal>
+
+
+
+
+      </>
     );
   }
 }
